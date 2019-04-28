1ff019ce15a4c38e064dbfdd91d325ae25b4474f
Shreyas Mohan <talk2shreyas@gmail.com>
2019-04-28 13:02:31 -0500

    Date fix (#247)
    * fix backend bug
    
    * fix undefined day

--- c2tc-mobile/screens/TipOverviewScreen.js
+++ c2tc-mobile/screens/TipOverviewScreen.js
@@ -159,13 +159,13 @@ class TipOverviewScreen extends React.Component {
     ];
 
     const dayNames = [
+      "Sunday",
       "Monday",
       "Tuesday",
       "Wednesday",
       "Thursday",
       "Friday",
-      "Saturday",
-      "Sunday"
+      "Saturday"
     ];
 
     const day = date.getDay();
@@ -173,11 +173,12 @@ class TipOverviewScreen extends React.Component {
     const year = date.getFullYear();
 
     const date_str =
-      dayNames[day - 1] +
+      dayNames[day] +
       " " +
       monthNames[monthIndex] +
       " " +
       year.toString().slice(2);
+    console.log(date_str)
     this.setState({
       currentdate: date_str
     });
